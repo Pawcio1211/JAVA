93be4471a06ff231bde615ac0b04fb0e750faeee
Paweł_Kwiatkowski <65865764+Pawcio1211@users.noreply.github.com>
2024-03-18 20:56:56 +0100

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,184 @@
+/**
+ * Hello
+ */
+public class Hello {
+
+    static int a = 6;
+    static float a1 = 6.0f;
+    static double a2 = 6.1d;
+    static char a3 = 'd';
+    static String a4 = "Hej";
+
+    public static void main(String[] args) {
+
+        String abecadlo[][] = {
+                { "Abecadlo z pieca spadlo" },
+                { "I o ziemie się rozjebalo" }
+        };
+        // System.out.println("No elo mordo\n"+abecadlo[0][0]);
+        // System.out.println("\n"+abecadlo[1][0]);
+    }
+}
+
+class ArrayCopyDemo {
+    public static void main(String[] args) {
+        String[] copyFrom = {
+                "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",
+                "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",
+                "Marocchino", "Ristretto" };
+
+        String[] copyTo = new String[7];
+        System.arraycopy(copyFrom, 2, copyTo, 0, 7);
+        for (String coffee : copyTo) {
+            System.out.print(coffee + ", ");
+        }
+    }
+}
+
+class ArrayCopyOfDemo {
+    public static void main(String[] args) {
+        String[] copyFrom = {
+                "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",
+                "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",
+                "Marocchino", "Ristretto" };
+
+        String[] copyTo = java.util.Arrays.copyOfRange(copyFrom, 2, 9);
+        for (String coffee : copyTo) {
+            System.out.print(coffee + " ");
+        }
+    }
+}
+
+class ArithmeticDemo {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2;
+        // result is now 3
+        System.out.println("1 + 2 = " + result);
+        int original_result = result;
+
+        result = result - 1;
+        // result is now 2
+        System.out.println(original_result + " - 1 = " + result);
+        original_result = result;
+
+        result = result * 2;
+        // result is now 4
+        System.out.println(original_result + " * 2 = " + result);
+        original_result = result;
+
+        result = result / 2;
+        // result is now 2
+        System.out.println(original_result + " / 2 = " + result);
+        original_result = result;
+
+        result = result + 8;
+        // result is now 10
+        System.out.println(original_result + " + 8 = " + result);
+        original_result = result;
+
+        result = result % 7;
+        // result is now 3
+        System.out.println(original_result + " % 7 = " + result);
+    }
+}
+
+class ComparisonDemo {
+
+    public static void main(String[] args) {
+        int value1 = 2;
+        int value2 = 2;
+        if (value1 == value2)
+            System.out.println("value1 == value2");
+        if (value1 != value2)
+            System.out.println("value1 != value2");
+        if (value1 > value2)
+            System.out.println("value1 > value2");
+        if (value1 < value2)
+            System.out.println("value1 < value2");
+        if (value1 <= value2)
+            System.out.println("value1 <= value2");
+    }
+}
+
+class ConditionalDemo2 {
+
+    public static void main(String[] args) {
+        int value1 = 1;
+        int value2 = 2;
+        int result;
+        boolean someCondition = false;
+        result = someCondition ? value1 : value2;
+
+        System.out.println(result);
+    }
+}
+
+class BreakDemo {
+    public static void main(String[] args) {
+
+        int[] arrayOfInts = { 32, 87, 3, 589,
+                12, 1076, 2000,
+                8, 622, 127 };
+        int searchfor = 12;
+
+        int i;
+        boolean foundIt = false;
+
+        for (i = 0; i < arrayOfInts.length; i++) {
+            if (arrayOfInts[i] == searchfor) {
+                foundIt = true;
+                break;
+            }
+        }
+        String o1 = "Found " + searchfor + " at index " + i;
+        String o2 = searchfor + " not in the array";
+        System.out.println(foundIt ? o1 : o2);
+
+    }
+}
+
+class Test {
+    enum Day {
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    }
+
+    public String calculate(Day d) {
+        return switch (d) {
+            case SATURDAY, SUNDAY -> "week-end";
+            default -> {
+                int remainingWorkDays = 5 - d.ordinal();
+                yield Integer.toString(remainingWorkDays);
+            }
+        };
+    }
+}
+
+
+class ContinueWithLabelDemo {
+    public static void main(String[] args) {
+
+        String searchMe = "Look for a substring in me";
+        String substring = "sub";
+        boolean foundIt = false;
+
+        int max = searchMe.length() -
+                  substring.length();
+
+    test:
+        for (int i = 0; i <= max; i++) {
+            int n = substring.length();
+            int j = i;
+            int k = 0;
+            while (n-- != 0) {
+                if (searchMe.charAt(j++) != substring.charAt(k++)) {
+                    continue test;
+                }
+            }
+            foundIt = true;
+                break test;
+        }
+        System.out.println(foundIt ? "Found it" : "Didn't find it");
+    }
+}
